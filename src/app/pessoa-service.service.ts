6648e98d6a1107c598eee35fff16e841df47cdc1
Felipe <felipeameno2@gmail.com>
2020-06-24 23:43:33 -0300

    Prova Final

--- src/app/pessoa-service.service.ts
+++ src/app/pessoa-service.service.ts
@@ -10,33 +10,13 @@ export class PessoaServiceService {
   constructor(private storage: Storage) { }
 
   async insert(user: Pessoa) {
-    const pessoa: Pessoa[] = (await this.storage.get("pessoa")) || [];
+    const pessoa: Pessoa[] = (await this.storage.get("pessoa")) || []
     user.id = pessoa.length + 1;
     return this.storage.set("pessoa", [...pessoa, user]);
   }
 
   async findId(id) {
-    const pessoa: Pessoa[] = (await this.storage.get("pessoa")) || [];
+    const pessoa: Pessoa[] = (await this.storage.get("pessoa")) || []
     return pessoa.find((pessoa) => pessoa.id == id);
   }
-
-  async
-
-
-
-}
-
-getPessoa(racaCor) {
-
-
-
-  console.log()
-
-}
-
-
-getPessoa1(id) {
-
-  console.log()
-}
 }
